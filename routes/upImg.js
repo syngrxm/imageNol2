002ebddcd68b4a2662abc733f3167d45adbc5625
syngrxm <smcubez48@gmail.com>
2023-06-22 18:02:59 +0900

    feat; upload sendImg

---
+++ routes/upImg.js
@@ -0,0 +1,9 @@
+const express = require("express"); // express 모듈 불러오기
+const app = express(); // express app 생성
+
+app.post("/image", (req, res) => {
+  const imgIn =
+    "/home/sumin/바탕화면/sendimganine/_F0ypmmjiqmdTETGiR4kZcVvoNFuF1Bo2XQXizafDyZtEbbc8YQP5qqZw7oPpZNhh07WBYoPPJbQkevd6bxGp9JP-E_AXIvI6nQZ9M8_AzPy3VCc-uwAdbFBreu45TWxebiPcDqiKGXOQmLiqMjc_A.webp";
+
+  res.sendFile(imgIn);
+});
