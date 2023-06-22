cd25aba4dd524b9339af86a533fa4bd27846da17
syngrxm <smcubez48@gmail.com>
2023-06-22 18:43:07 +0900

    edit Path

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-
-const imgupload = require("/home/sumin/바탕화면/sendimganine/routes/sendImg.js");
-const imgdwload = require("/home/sumin/바탕화면/sendimganine/routes/getImg.js");
+home / sumin / 바;
+const imgupload = require("/home/sumin/바탕화면/imageNol2/routes/upImg.js");
+const imgdwload = require("/home/sumin/바탕화면/imageNol2/routes/dwImg.js");
 
 app.get("/", (req, res) => {
   res.send("F.i.l.e.을 보내고 받아 보께요");
