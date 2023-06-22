6b620f5e6bc80751839e0b8faeb09f85a8d18a48
syngrxm <smcubez48@gmail.com>
2023-06-22 18:40:42 +0900

    feat:오타 수정!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 const imgupload = require("/home/sumin/바탕화면/sendimganine/routes/sendImg.js");
-const imgdwload = requie("/home/sumin/바탕화면/sendimganine/routes/getImg.js");
+const imgdwload = require("/home/sumin/바탕화면/sendimganine/routes/getImg.js");
 
 app.get("/", (req, res) => {
   res.send("F.i.l.e.을 보내고 받아 보께요");
@@ -14,5 +14,5 @@ app.use("", imgdwload);
 app.use("", imgupload);
 
 app.listen(app.get("port"), () => {
-  console.log(app.post("port"), "번 포트에서 대기 중");
+  console.log(app.port("port"), "번 포트에서 대기 중");
 });
