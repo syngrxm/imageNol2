c3dc523b6aad9da62aa6733b261185c64f308d69
syngrxm <smcubez48@gmail.com>
2023-06-29 03:05:56 +0900

    feat: 유동적인 어쩌구

--- routes/dwImg.js
+++ routes/dwImg.js
@@ -1,11 +1,12 @@
 const express = require("express"); // express 모듈 불러오기
 const router = express.Router();
 
-router.post("/get_image", (req, res) => {
-  const imgURL =
-    "https://mblogthumb-phinf.pstatic.net/20160106_209/kukjeart_inc_1452055876208XMVrB_JPEG/%C5%E4%B3%A2_%285%29.jpg?type=w800";
+router.use(express.json());
 
-  res.send(`<img src="${imgURL}" alt="Image">`);
+router.post("/get_image", (req, res) => {
+  const { imgLink } = req.body;
+
+  res.send(`<img src="${imgLink}" alt="Image">`);
 });
 
 module.exports = router;
