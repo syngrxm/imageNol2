a39a73e17bb8ecef5c786cd54f6239aae438d838
syngrxm <smcubez48@gmail.com>
2023-06-30 23:29:56 +0900

    feat:오류찐막수정하아아아json

--- routes/upImg.js
+++ routes/upImg.js
@@ -1,10 +1,14 @@
-const express = require("express"); // express 모듈 불러오기
+const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+router.use(express.json());
 
 router.post("/image", (req, res) => {
-  const imgIn = "/home/sumin/바탕화면/imageNol2/rabbit.webp";
+  const { imgPath } = req.body;
+  const filePath = path.resolve(__dirname, imgPath);
 
-  res.sendFile(imgIn);
+  res.sendFile(filePath);
 });
 
 module.exports = router;
