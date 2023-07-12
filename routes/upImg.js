6637f5731136bedb73d199d999c558b9c6bb5eea
syngrxm <smcubez48@gmail.com>
2023-07-12 16:06:05 +0900

    d

--- routes/upImg.js
+++ routes/upImg.js
@@ -1,14 +1,32 @@
 const express = require("express");
-const router = express.Router();
+const fs = require("fs"); /// fs = file system
 const path = require("path");
+const multer = require("multer");
+const router = express.Router();
 
 router.use(express.json());
 
-router.post("/image", (req, res) => {
-  const { imgPath } = req.body;
-  const filePath = path.resolve(__dirname, imgPath);
+const upload = multer({
+  dest: path.join(__dirname, "uploads"),
+});
 
-  res.sendFile(filePath);
+router.post("/image", upload.single("flie"), (req, res) => {
+  const { imgName } = req.body;
+  const img = req.file.path;
+  const imgEcd = Buffer.from(img, "base64");
+  const imgDcd = imgEcd.toString("base64");
+  const imgPath = path.join(__dirname, "uploads", imgName);
+
+  fs.writeFile(imgPath, imgDcd, "base64", (err) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ errormessage: "이미지 저장 중 오류가 발생하였습니다." });
+    } else {
+      status: "성공!";
+    }
+  });
 });
 
 module.exports = router;
